346ed95dffeb66a562a8102ed2d97fab6d2b96a1
cameroncordero <cameroncordero@gmail.com>
2021-10-22 12:55:25 -0700

    ben clean up

--- SequenceContext.py
+++ SequenceContext.py
@@ -1,21 +1,9 @@
-MutSeq = open('Cutaneous_somatic_mutation_MELA_AU_DNVs_sorted_dinuc_filter.bed', 'r')
-MutSeqWithContext = open('Cutaneous_somatic_mutation_with_updown_context.bed', 'w')
-for i,entry in enumerate(MutSeq):
-    lineColumns = entry.strip().split('\t')
-    mutStart = int(lineColumns[1]) - 1
-    mutEnd = int(lineColumns[2]) + 1
-    MutSeqWithContext.write(str(lineColumns[0]))
-    MutSeqWithContext.write('\t')
-    MutSeqWithContext.write(str(mutStart))
-    MutSeqWithContext.write('\t')
-    MutSeqWithContext.write(str(mutEnd))
-    MutSeqWithContext.write('\t')
-    MutSeqWithContext.write(str(lineColumns[3]))
-    MutSeqWithContext.write('\t')
-    MutSeqWithContext.write(str(lineColumns[4]))
-    MutSeqWithContext.write('\t')
-    MutSeqWithContext.write(str(lineColumns[5]))
-    MutSeqWithContext.write('\n')
-MutSeqWithContext.close()
-MutSeq.close()
+with open('Cutaneous_somatic_mutation_MELA_AU_DNVs_sorted_dinuc_filter.bed', 'r') as MutSeq:
+    with open('Cutaneous_somatic_mutation_with_updown_context.bed', 'w') as MutSeqWithContext:
+        for line in MutSeq:
+            lineColumns = line.strip().split('\t')
+            mutStart = int(lineColumns[1]) - 1
+            mutEnd = int(lineColumns[2]) + 1
+            MutSeqWithContext.write('\t'.join( [lineColumns[0], str(mutStart), 
+                str(mutEnd)] + lineColumns[3:] ) + '\n')
 print('done.')
