cf09af7e1d0565589b883c32eb871d9215bcc369
cameroncordero <cameroncordero@gmail.com>
2021-11-01 14:30:47 -0700

    Graph is DONE! Next is streamline

--- CreateGraph.py
+++ CreateGraph.py
@@ -2,4 +2,5 @@ import seaborn as sns
 import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
-filename = 'normalizedMutData.csv'
+gdata = pd.read_csv('normalizedMutData.csv', header = None, names= ['Mutation' , 'Frequency'])
+sns.barplot(x = 'Mutation', y = 'Frequency', data = gdata,))
