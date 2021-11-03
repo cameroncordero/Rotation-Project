8de52bb9acebbc29db361a2408e7ca372a201afb
cameroncordero <cameroncordero@gmail.com>
2021-11-03 13:15:42 -0700

    clean up files

--- ActuallyNormalizeData.py
+++ ActuallyNormalizeData.py
@@ -1,27 +1,54 @@
-import statistics
-with open('mutationData.csv', 'r') as mutData:
-    with open('genomicData.csv', 'r') as gData:
-        with open('normalizedMutData.csv', 'w') as normData:
-            mutDataDict = {}
-            gDataDict = {}
-            normDataDict = {}
-            finalDataDict = {}
-            # re-writing dictionaries from files
-            for line1 in mutData:
-                sepLine = line1.strip().split(',')
-                mutDataDict[sepLine[0]] = sepLine[1]
-            for line2 in gData:
-                sepLine = line2.strip().split(',')
-                gDataDict[sepLine[0]] = sepLine[1]
-            # dividing the frequency of mutation 4-mer sequences
-                # by the frequency of genomic 4-mer sequences 
-            for mut1, mfreq1 in mutDataDict.items():
-                gfreq = gDataDict.get(mut1)
-                normDataDict[mut1] = int(mfreq1)/int(gfreq)
-            # dividing the data by the median to have a value of 1
-            dataMedian = statistics.median(normDataDict.values())
-            for mut2, mfreq2 in normDataDict.items():
-                finalDataDict[mut2] = mfreq2#/dataMedian
-            for key, value in finalDataDict.items():
-                normData.write
-                normData.write('%s,%s\n' % (key, value))
+def NormalizeData(mutationSelection = True, mutation: str, ignorePoorlyDefindRegions = True,
+    MutationFile: str, FastaFile: str, OutputFile: str)
+    import time
+    import statistics
+    start_time = time.time()
+    with open('genomicData.csv', 'w') as gData:
+        with open('hg19.fa', 'r') as genome:
+            seqCounts = dict()
+            for line in genome:
+                if '>' in line:
+                    print ('\x1B[3mProcessing...\x1B[0m ' + 
+                        line[1:4] + 'omosome ' + line[4:] +
+                        '%s seconds since start' % (time.time() - start_time))
+                    fullLine = ''
+                    leftovers = ''
+                    continue
+                else:
+                    i = 0
+                    fullLine = leftovers + line.strip()
+                    fourNuc = fullLine[i:i+4]
+                    while len(fourNuc) > 3:
+                        seqCounts[fourNuc] = seqCounts.setdefault(fourNuc, 0) + 1
+                        i += 1
+                        fourNuc = fullLine[i:i+4]
+                    leftovers = fullLine[i:]
+            for key, value in seqCounts.items():
+                gData.write('%s,%s\n' % (key, value))
+    print('%s seconds to process' % (time.time() - start_time))
+    with open(MutationFile, 'r') as mutData:
+        with open(FastaFile, 'r') as gData:
+            with open('normalizedMutData.csv', 'w') as normData:
+                mutDataDict = {}
+                gDataDict = {}
+                normDataDict = {}
+                finalDataDict = {}
+                # re-writing dictionaries from files
+                for line1 in mutData:
+                    sepLine = line1.strip().split(',')
+                    mutDataDict[sepLine[0]] = sepLine[1]
+                for line2 in gData:
+                    sepLine = line2.strip().split(',')
+                    gDataDict[sepLine[0]] = sepLine[1]
+                # dividing the frequency of mutation x-mer sequences
+                    # by the frequency of genomic x-mer sequences 
+                for mut1, mfreq1 in mutDataDict.items():
+                    gfreq = gDataDict.get(mut1)
+                    normDataDict[mut1] = int(mfreq1)/int(gfreq)
+                # dividing the data by the median to have a value of 1
+                dataMedian = statistics.median(normDataDict.values())
+                for mut2, mfreq2 in normDataDict.items():
+                    finalDataDict[mut2] = mfreq2/dataMedian
+                for key, value in finalDataDict.items():
+                    normData.write
+                    normData.write('%s,%s\n' % (key, value))
--- SequenceContext.py
+++ SequenceContext.py
@@ -1,10 +1,37 @@
-with open('Cutaneous_somatic_mutation_MELA_AU_DNVs_sorted_dinuc_filter.bed', 'r') as MutSeq:
-    with open('Cutaneous_somatic_mutation_with_updown_context.bed', 'w') as MutSeqWithContext:
-        for line in MutSeq:
-            lineColumns = line.strip().split('\t')
-            mutStart = int(lineColumns[1]) - 1
-            mutEnd = int(lineColumns[2]) + 1
-            MutSeqWithContext.write('\t'.join( [lineColumns[0], str(mutStart), 
-                str(mutEnd)] + lineColumns[3:] ) + '\n')
-print('done.')
-
+def CreateNewBedFile(MutationFile: str, FastaFile: str, OutputFile: str, fivePrimeCont: int, threePrimeCont: int):
+    """
+    Takes a .bed file and returns another bed file that has increased nucleotide context. (numerical values remain the same)
+    The context can be increased by fivePrimeCount and threePrimeCount.
+    Input the file names once they're in the same directory.
+    """    
+    with open(MutationFile, 'r') as MutSeq:
+        with open('MutationFileWithContext.bed', 'w') as MutSeqWithContext:
+                for line in MutSeq:
+                    lineColumns = line.strip().split('\t')
+                    # decrease and increase the positional numbers so when
+                    # compared to the fasta file, it will return the nucleotides
+                    # that are in the newly defined range
+                    mutStart = int(lineColumns[1]) - fivePrimeCont
+                    mutEnd = int(lineColumns[2]) + threePrimeCont
+                    # write those new position numbers to 
+                    MutSeqWithContext.write('\t'.join(lineColumns[0] +
+                        str(mutStart) + str(mutEnd) + lineColumns[3:] )
+                        + '\n')
+    # terminal portion to compare with fasta file
+    #with open('MutationFileWithContext.bed', 'r') as MutSeqWithContext:
+    # output file from terminal fasta comparison
+    with open(MutationFile, 'r') as MutSeq:
+        with open(OutputFile, 'w') as MutSeqPostFasta:
+            with open(FastaFile, 'r') as FastaReturn:
+                for line in MutSeq:
+                    lineColumns = line.strip().split('\t')
+                    mutStart = int(lineColumns[1]) - fivePrimeCont
+                    mutEnd = int(lineColumns[2]) + threePrimeCont
+                    mutText = FastaReturn.readline()
+                    check = re.split(':|-|\(', mutText, maxsplit = 3)
+                    plusMinus = check[3]
+                    # checks to make sure all lines are present and have accurate counts
+                    assert (check[0][1:] == lineColumns[0] and check[1] == str(pos1) and 
+                        check[2] == str(pos2) and plusMinus[0] == lineColumns[5]), line + check
+                    MutSeqPostFasta.write('\t'.join( lineColumns[:3] + 
+                        [FastaReturn.readline().strip()] + lineColumns[4:] ) + '\n' )
