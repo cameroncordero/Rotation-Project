82fed45011ad5489b4f74a1b275cd545d53a4b6e
cameroncordero <cameroncordero@gmail.com>
2021-11-05 10:56:03 -0700

    FINISHED

--- ActuallyNormalizeData.py
+++ ActuallyNormalizeData.py
@@ -1,5 +1,3 @@
-from decimal import Context
-import time
 import os
 import statistics
 def NormalizeData(OutputFile, FastaFile, TotalNucleotidesPerContext: int, NormalizeToMedian = True):
@@ -7,12 +5,13 @@ def NormalizeData(OutputFile, FastaFile, TotalNucleotidesPerContext: int, Normal
     Normalizes data to the genome and median default is true.
     """
     if not os.path.exists('Genomic_Counts_'+FastaFile):
-        start_time = time.time()
         with open('Genomic_Counts_'+FastaFile, 'w') as gData:
             with open(FastaFile, 'r') as genome:
                 seqCounts = dict()
                 for line in genome:
                     if '>' in line:
+                        print ('\x1B[3mProcessing...\x1B[0m ' + 
+                            line[1:4] + 'omosome ' + line[4:])
                         fullLine = ''
                         leftovers = ''
                         continue
@@ -20,7 +19,7 @@ def NormalizeData(OutputFile, FastaFile, TotalNucleotidesPerContext: int, Normal
                         i = 0
                         fullLine = leftovers + line.strip()
                         nucContext = fullLine[i:i+TotalNucleotidesPerContext]
-                        while len(nucContext) > TotalNucleotidesPerContext+1:
+                        while len(nucContext) > (TotalNucleotidesPerContext-1):
                             seqCounts[nucContext] = seqCounts.setdefault(nucContext, 0) + 1
                             i += 1
                             nucContext = fullLine[i:i+4]
@@ -29,16 +28,18 @@ def NormalizeData(OutputFile, FastaFile, TotalNucleotidesPerContext: int, Normal
                 for key, value in seqCounts.items():
                     gData.write('%s,%s\n' % (key, value))
     with open(OutputFile, 'r') as mutData:
-        with open('Genomic_Counts_'+FastaFile, 'r') as gData:
+        with open('Genomic_Counts_'+TotalNucleotidesPerContext+'_mer'+FastaFile, 'r') as gData:
             with open('Normalized_'+OutputFile, 'w') as normData:
                 mutDataDict = {}
                 gDataDict = {}
                 normDataDict = {}
                 finalDataDict = {}
                 # re-writing dictionaries from files
+                mutData = mutData.readlines()[1:]
                 for line1 in mutData:
                     sepLine = line1.strip().split(',')
                     mutDataDict[sepLine[0]] = sepLine[1]
+                gData = gData.readlines()[1:]
                 for line2 in gData:
                     sepLine = line2.strip().split(',')
                     gDataDict[sepLine[0]] = sepLine[1]
@@ -52,6 +53,6 @@ def NormalizeData(OutputFile, FastaFile, TotalNucleotidesPerContext: int, Normal
                 if NormalizeToMedian == True:
                     for mut2, mfreq2 in normDataDict.items():
                         finalDataDict[mut2] = mfreq2/dataMedian
+                normData.write('sequenceContext,normalizedCounts'+'\n')
                 for key, value in finalDataDict.items():
-                    normData.write
                     normData.write('%s,%s\n' % (key, value))
